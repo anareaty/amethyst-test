997a87b425c4dbcdad3700ba5082a62585ecd91b
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2024-05-08 12:29:20 +0400

    Update mastodon.js

--- assets/mastodon.js
+++ assets/mastodon.js
@@ -54,7 +54,7 @@ fetch(mastodonProfile + '.rss')
 
       const dateElement = document.createElement('p');
       const pubDate = new Date(item.querySelector("pubDate").textContent);
-      dateElement.textContent = `${"Опубликовано"} ${pubDate.toLocaleDateString()} - ${pubDate.toLocaleTimeString().slice(0,5)} | `;
+      dateElement.textContent = `${""} ${pubDate.toLocaleDateString()} - ${pubDate.toLocaleTimeString().slice(0,5)} | `;
       footerElement.appendChild(dateElement);
 
       const linkElement = document.createElement('a');
